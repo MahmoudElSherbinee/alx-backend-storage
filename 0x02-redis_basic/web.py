008b01d74af533f25078131e18dea47111c169f9
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-08-14 20:54:00 +0300

    fix adv 3

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,33 +6,29 @@ import requests
 from functools import wraps
 from typing import Callable
 
-# Initialize the Redis client
+
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
 
+
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data and tracks the number of accesses.
+    '''Caches the output of fetched data.
     '''
     @wraps(method)
     def invoker(url) -> str:
-        '''The wrapper function for caching the output and tracking accesses.
+        '''The wrapper function for caching the output.
         '''
-        # Increment the access count for the URL
         redis_store.incr(f'count:{url}')
-        
-        # Check if the result is already cached
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        
-        # Fetch and cache the result if not already cached
         result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
-        
         return result
-
     return invoker
+
 
 @data_cacher
 def get_page(url: str) -> str:
